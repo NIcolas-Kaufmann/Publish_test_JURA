5cbb0ad4622431929f9d7d5bc2d6e9861f1e7c09
Nicolas Kaufmann <ayann@Someones-MacBook-Pro.local>
2023-05-09 12:14:54 +0200

    a

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name = "rv_curve_jura",
     version = "0.1",
@@ -7,6 +10,9 @@ setuptools.setup(
     description = "test module",
     author_email= "nicolas.kaufmann@unibe.ch",
     packages=setuptools.find_packages(),
+    long_description = long_description,
+    long_description_content_tyype="text/markdown",
+    url = "https://github.com/NIcolas-Kaufmann/Publish_test_JURA",
     install_requires = ["numpy", "matplotlib"],
     classifiers = ["Programming Language :: Python :: 3",
                     "Operating System :: OS Independent"
